c85b05d8d91ede2f7888da7b9fb0e0e384f0d6d4
marianobonelli <mbonelli95@gmail.com>
2023-12-18 19:44:15 -0300

    modificación diccionario traducción

--- language.py
+++ language.py
@@ -1,100 +1,38 @@
 translate_dict = {
-    # Traducciones Título
-    "title":{"es": "Tablero de Lotes y Cultivos", "en": "Fields and Crops Dashboard", "pt": "Painel de Campos e Cultivos"},
-    "requested_by": {"es": "Solicitado por: ", "en": "Requested by: ", "pt": "Solicitado por: "},
-    "logout": {"es": "Cerrar Sesión", "en": "Logout", "pt": "Sair"},
-    "language": {"es": "Idioma", "en": "Language", "pt": "Idioma"},
-    "select_all": {"es": "Seleccionar todo", "en": "Select all", "pt": "Selecionar tudo"},
-
-    # Traducciones Selector de color
-    "color_palette": {"es": "Paleta de colores", "en": "Color Palette", "pt": "Paleta de Cores"},
-
-    # Traducciones Area
-    "area": {"es": "Área", "en": "Area", "pt": "Área"},
-    "choose_option": {"es": "Elije una opción", "en": "Choose an option", "pt": "Escolha uma opção"},
-
-    # Traducciones Workspace
-    "workspace": {"es": "Espacio de Trabajo", "en": "Work Space", "pt": "Espaço de Trabalho"},  
-
-    # Traducciones Season
-    "season": {"es": "Campaña", "en": "Season", "pt": "Temporada"},
-
-    # Traducciones Farm
-    "farm": {"es": "Establecimiento", "en": "Farm", "pt": "Fazenda"},
-    "farms": {"es": "Establecimientos", "en": "Farms", "pt": "Fazendas"},
-
-    # Traducciones Field
-    "field": {"es": "Lote", "en": "Field", "pt": "Campo"},
-    "fields": {"es": "Lotes", "en": "Fields", "pt": "Campos"},
-
-    # Traducciones Cultivo
-    "crop": {"es": "Cultivo", "en": "Crop", "pt": "Cultivo"},
-    "crops": {"es": "Cultivos", "en": "Crops", "pt": "Cultivos"},
-
-    # Traducciones Híbrido
-    "hybrid_variety": {"es": "Híbrido / Variedad", "en": "Hybrid / Variety", "pt": "Híbrido / Variedade"},
-    "hybrid_varieties": {"es": "Híbridos / Variedades", "en": "Hybrids / Varieties", "pt": "Híbridos / Variedades"},    
-
-    "hectares": {"es": "Hectáreas", "en": "Hectares", "pt": "Hectares"},
-
-    # Traducciones para "lotes sin cultivos"
-    "fields_without_crops": {
-        "es": "Lotes sin cultivos",
-        "en": "Fields without crops",
-        "pt": "Campos sem cultivos"
-    },
-
-    # Traducciones para "lotes sin híbridos o variedades"
-    "fields_without_hybrids_or_varieties": {
-        "es": "Lotes sin híbridos/variedades",
-        "en": "Fields without hybrids/varieties",
-        "pt": "Campos sem híbridos/variedades"
-    },
-
-    # Traducciones para "lotes sin fecha de siembra"
-    "fields_without_sowing_date": {
-        "es": "Lotes sin fecha de siembra",
-        "en": "Fields without sowing date",
-        "pt": "Campos sem data de plantio"
-    },
-
-    "metrics": {"es": "Métricas", "en": "Metrics", "pt": "Métricas"},
-
-    # Traducciones Capas
-    "yield_layers": {"es": "Capas de rindes", "en": "Yield Layers", "pt": "Camadas de Rendimento"},
-
-    # Traducciones Gráfico
-    "select_grouping_field": {"es": "Seleccione un campo de agrupamiento:", "en": "Select a grouping field:", "pt": "Selecione um campo de agrupamento:"},
-    "hectares_by": {"es": "Hectáreas por", "en": "Hectares by", "pt": "Hectares por"},
-    "yield_layer": {"es": "Capa de Rinde", "en": "Yield Layer", "pt": "Camada de Rendimento"},
-    "adjusted_average_yield": {"es": "Rendimiento Medio Ajustado", "en": "Adjusted Average Yield", "pt": "Rendimento Médio Ajustado"},
-    "average_yield" : {"es": "Rendimiento Medio", "en": "Average Yield", "pt": "Rendimento Médio"},
-
-    # Traducciones Gráfico 2
-    "weighted_yield_by": {"es": "Rendimiento Ponderado por", "en": "Weighted Yield by", "pt": "Rendimento Ponderado por"},
-    "weighted_yield": {"es": "Rendimiento Ponderado", "en": "Weighted Yield", "pt": "Rendimento Ponderado"},
-
-    # Traducciones mapa
-    "point_map_by_field_according_to": {"es": "Mapa de lotes según", "en": "Point Map by Field According to", "pt": "Mapa de Pontos por Campo Segundo"},
-        
-    # Campos
-    "area_field": {"es": "Área", "en": "Area", "pt": "Área"},
-    "ws_field": {"es": "Espacio de Trabajo", "en": "Work Space", "pt": "Espaço de Trabalho"}, 
-    "season_field": {"es": "Temporada", "en": "Season", "pt": "Temporada"},
-    "farm_field": {"es": "Establecimiento", "en": "Farm", "pt": "Estabelecimento"},
-
-    "crop_field": {"es": "Cultivo", "en": "Crop", "pt": "Cultivo"},
-    "hybrid_variety_field": {"es": "Híbrido / Variedad", "en": "Hybrid / Variety", "pt": "Híbrido / Variedade"},
-    "adjusted_average_yield_field": {"es": "Rendimiento Medio Ajustado", "en": "Adjusted Average Yield", "pt": "Rendimento Médio Ajustado"},
-
-    "select_yield_layer_warning": {"es": "Debe seleccionar una capa de rindes para continuar", "en": "You must select a yield layer to continue", "pt": "Você deve selecionar uma camada de rendimento para continuar"},
-
-    "download_csv": {"es" : "Descargar CSV", "en":"Download CSV", "pt":"Baixar CSV"},
-
-    "heatmap": {"es": "Heatmap por Hectáreas", "en": "Heatmap by Hectares", "pt": "Heatmap por Hectares"},
-
-    "seeding_date": {'es': 'Fecha de siembra', 'en': 'Seeding Date', 'pt': 'Data de Semeadura'},
-
-    "seeding_date_by": {'es': 'Fecha de siembra por ', 'en': 'Seeding Date by ', 'pt': 'Data de Semeadura por '}      
-
+    "area": {"en": "Area", "es": "Área", "pt": "Área"},
+    "area_field": {"en": "Area", "es": "Área", "pt": "Área"},
+    "choose_option": {"en": "Choose an option", "es": "Elije una opción", "pt": "Escolha uma opção"},
+    "color_palette": {"en": "Color Palette", "es": "Paleta de colores", "pt": "Paleta de Cores"},
+    "crop": {"en": "Crop", "es": "Cultivo", "pt": "Cultivo"},
+    "crop_field": {"en": "Crop", "es": "Cultivo", "pt": "Cultivo"},
+    "crops": {"en": "Crops", "es": "Cultivos", "pt": "Cultivos"},
+    "download_csv": {"en": "Download CSV", "es": "Descargar CSV", "pt": "Baixar CSV"},
+    "farm": {"en": "Farm", "es": "Establecimiento", "pt": "Fazenda"},
+    "farm_field": {"en": "Farm", "es": "Establecimiento", "pt": "Estabelecimento"},
+    "farms": {"en": "Farms", "es": "Establecimientos", "pt": "Fazendas"},
+    "field": {"en": "Field", "es": "Lote", "pt": "Campo"},
+    "fields": {"en": "Fields", "es": "Lotes", "pt": "Campos"},
+    "fields_without_crops": {"en": "Fields without crops", "es": "Lotes sin cultivos", "pt": "Campos sem cultivos"},
+    "fields_without_hybrids_or_varieties": {"en": "Fields without hybrids/varieties", "es": "Lotes sin híbridos/variedades", "pt": "Campos sem híbridos/variedades"},
+    "fields_without_sowing_date": {"en": "Fields without sowing date", "es": "Lotes sin fecha de siembra", "pt": "Campos sem data de plantio"},
+    "heatmap": {"en": "Heatmap by Hectares", "es": "Heatmap por Hectáreas", "pt": "Heatmap por Hectares"},
+    "hectares": {"en": "Hectares", "es": "Hectáreas", "pt": "Hectares"},
+    "hectares_by": {"en": "Hectares by", "es": "Hectáreas por", "pt": "Hectares por"},
+    "hybrid_varieties": {"en": "Hybrids / Varieties", "es": "Híbridos / Variedades", "pt": "Híbridos / Variedades"},
+    "hybrid_variety": {"en": "Hybrid / Variety", "es": "Híbrido / Variedad", "pt": "Híbrido / Variedade"},
+    "hybrid_variety_field": {"en": "Hybrid / Variety", "es": "Híbrido / Variedad", "pt": "Híbrido / Variedade"},
+    "language": {"en": "Language", "es": "Idioma", "pt": "Idioma"},
+    "logout": {"en": "Logout", "es": "Cerrar Sesión", "pt": "Sair"},
+    "metrics": {"en": "Metrics", "es": "Métricas", "pt": "Métricas"},
+    "point_map_by_field_according_to": {"en": "Point Map by Field According to", "es": "Mapa de lotes según", "pt": "Mapa de Pontos por Campo Segundo"},
+    "requested_by": {"en": "Requested by: ", "es": "Solicitado por: ", "pt": "Solicitado por: "},
+    "season": {"en": "Season", "es": "Campaña", "pt": "Temporada"},
+    "season_field": {"en": "Season", "es": "Temporada", "pt": "Temporada"},
+    "seeding_date": {"en": "Seeding Date", "es": "Fecha de siembra", "pt": "Data de Semeadura"},
+    "seeding_date_by": {"en": "Seeding Date by ", "es": "Fecha de siembra por ", "pt": "Data de Semeadura por "},
+    "select_all": {"en": "Select all", "es": "Seleccionar todo", "pt": "Selecionar tudo"},
+    "select_grouping_field": {"en": "Select a grouping field:", "es": "Seleccione un campo de agrupamiento:", "pt": "Selecione um campo de agrupamento:"},
+    "title": {"en": "Fields and Crops Dashboard", "es": "Tablero de Lotes y Cultivos", "pt": "Painel de Campos e Cultivos"},
+    "workspace": {"en": "Work Space", "es": "Espacio de Trabajo", "pt": "Espaço de Trabalho"},
+    "ws_field": {"en": "Work Space", "es": "Espacio de Trabajo", "pt": "Espaço de Trabalho"}, 
 }
